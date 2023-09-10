3919a6ddd8cd010512a2ab35756f3d4914cf6a8a
Rajajayaruban <rajajayaruban@virtualmaze.co.in>
2023-09-10 18:37:36 +0530

    First commit

--- frontend/src/config/reverseProxy.js
+++ frontend/src/config/reverseProxy.js
@@ -3,7 +3,7 @@ export default {
 
     'appProxy': `/app`,
     'proxyUrl': `/api`,
-    'servicesUrl': `http://localhost:8080/api`,
+    'servicesUrl': `https://prooctor-services.onrender.com/api`,
     'OpenText': "https://na-1-dev.api.opentext.com",
     'OpenText_CS': "https://css.na-1-dev.api.opentext.com",
 }
